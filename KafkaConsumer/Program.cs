fc694b4af31b3f746c932ac41bc519858cf71be8
kinbech@gmail.com <kinbech@gmail.com>
2018-10-07 09:52:03 -0500

    first commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        private const int MillisecondsTimeout = 100;
+
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "sample-consumer" },
+                { "bootstrap.servers", "localhost:9092" },
+                { "enable.auto.commit", "false"}
+            };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {                
+                consumer.Subscribe(new string[]{"hello-topic"});
+
+                consumer.OnMessage += (_, msg) => 
+                {
+                Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                consumer.CommitAsync(msg);
+                };
+
+                while (true)
+                {
+                    consumer.Poll(MillisecondsTimeout);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        private const int MillisecondsTimeout = 100;
+
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            }; 
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = null;
+
+                while (text != "exit")
+                {
+                text = Console.ReadLine();                                                                                                                                                              
+                producer.ProduceAsync("hello-topic", null, text);
+                }
+
+                producer.Flush(MillisecondsTimeout);
+            }
+        }
+    }
+}
